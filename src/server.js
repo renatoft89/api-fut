6207fab5cc1f3cc36505bb8b4d7f2dde57d05b5f
Renato <renatoft89@gmail.com>
2022-06-27 10:25:43 -0300

    fix: fix PORT server

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ const app = require('./api');
 
 const PORT = process.env.PORT || 3002;
 
-app.listen(PORT, () => console.log('Online Port: ', PORT, 'http://localhost:3002/'));
+app.listen(PORT, () => console.log('Online Port: ', PORT, `http://localhost:${PORT}/`));
