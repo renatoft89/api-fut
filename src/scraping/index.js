7900c72f6065404b986f12f60bf10c1eef017444
Renato <renatoft89@gmail.com>
2022-08-01 19:43:40 -0300

    feat: add numero de jogos

--- src/scraping/index.js
+++ src/scraping/index.js
@@ -9,11 +9,12 @@ const tabela = async () => {
       const clubes = Array.from(document.querySelectorAll('table tr td span.hidden-xs', table => table.textContent)).map(club => club.innerText); 
       const pontos = Array.from(document.querySelectorAll('table tr.expand-trigger th', table => table.textContent)).map(pt => pt.innerText);
       const escudos = Array.from(document.querySelectorAll('table tr.expand-trigger td img.icon.escudo.m-r-10', table => table.textContent)).map(img => img.getAttribute('src'));
-
+      const jogos = Array.from(document.querySelectorAll('table tr.expand-trigger td:nth-child(3)', table => table.textContent)).map(jogos => jogos.innerText);
       return {
         clubes,
         pontos, 
-        escudos }
+        escudos,
+        jogos }
     })
     await browser.close();
     return tabelaSerieA;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./api');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3009;
 
-app.listen(PORT, () => console.log('Online Port: ', PORT, `http://localhost:${PORT}/`));
+app.listen(PORT, () => console.log('Online Port: ', PORT, `http://localhost:${PORT}/brasileirao`));
